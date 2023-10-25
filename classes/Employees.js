823a97e9cfb2ea6332f004c665a996c01b3b6480
elliottdinley <elliott.dinley1@gmail.com>
2023-10-25 11:56:36 +0100

    complete w/ extensions

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,8 +1,102 @@
 class Employees {
+    static allEmployees = [];
+    #salary;
+    #isHired;
 
+    // Performance Metrics
+    #salesNumbers;
+    #projectCompletionRate;
+    #employeeRetentionRate;
 
+    constructor(name, position, salary) {
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        this.#isHired = true;
 
-    
+        Employees.allEmployees.push(this);
+
+        // Performance Metrics
+        this.#salesNumbers = 0;
+        this.#projectCompletionRate = 0;
+        this.#employeeRetentionRate = 0;
+    }
+
+    getSalary() {
+        return this.#salary;
+    }
+    setSalary(amount) {
+        // Error Handling
+        if (amount < 0) {
+            throw new Error("Salary cannot be negative")
+        }
+
+        this.#salary = amount
+    }
+
+    getStatus() {
+        return this.#isHired;
+    }
+    setStatus(command) {
+        this.#isHired = (command === "hire");
+    }
+
+    static getEmployees() {
+        return Employees.allEmployees;
+    }
+
+    static getTotalPayroll() {
+        return Employees.allEmployees.reduce((acc, val) => acc + val.getSalary(), 0)
+    }
+
+    // Promotions
+    promote(title, salary) {
+        this.position = title;
+        this.#salary = salary;
+    }
+
+    // Performance Metrics
+    setSalesNumbers(numbers) {
+        this.#salesNumbers = numbers;
+    }
+    getSalesNumbers() {
+        return this.#salesNumbers;
+    }
+
+    setProjectCompletionRate(rate) {
+        this.#projectCompletionRate = rate;
+    }
+    getProjectCompletionRate() {
+        return this.#projectCompletionRate;
+    }
+
+    setEmployeeRetentionRate(rate) {
+        this.#employeeRetentionRate = rate;
+    }
+    getEmployeeRetentionRate() {
+        return this.#employeeRetentionRate;
+    }
+
+    calculatePerformanceScore() {
+        if (this.position === "Salesperson") {
+            return this.#salesNumbers * 0.1;
+        } else if (this.position === "Software Engineer") {
+            return this.#projectCompletionRate * 0.2;
+        } else if (this.position === "Manager") {
+            return this.#employeeRetentionRate * 0.3;
+        }
+        return 0;
+    }
+
+    determineBonusesAndPromotions() {
+        const performanceScore = this.calculatePerformanceScore();
+
+        if (performanceScore > 50) {
+            console.log(`${this.name} is eligible for a bonus or promotion.`);
+        } else {
+            console.log(`${this.name} does not meet the criteria for a bonus or promotion.`);
+        }
+    }
 }
 
 module.exports = {
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,8 +1,26 @@
-class SalesPerson {
+const { Employees } = require("./Employees.js");
 
+class SalesPerson extends Employees {
+    #totalSales;
 
+    constructor(name, position, salary, clients) {
+        super(name, position, salary);
+        this.clients = clients;
+        this.#totalSales = 0;
+    }
 
-    
+    getSalesNumber() {
+        return this.#totalSales;
+    }
+    makeSale(amount) {
+        this.#totalSales += amount;
+    }
+
+    // Search
+    findClient(client) {
+        return this.clients.includes(client);
+    }
+
 }
 
 module.exports = {
